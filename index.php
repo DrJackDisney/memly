02a4f53e2a8ae9469009e120c6053adb23aa178e
inaba_t <inaba_t@worksap.co.jp>
2017-01-13 13:33:56 +0900

    start memly

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Memly</title>
+</head>
+<body>
+<?php
+$message = "Hello Memly";
+echo "<h1>$message</h1>";
+?>
+</body>
+</html>
