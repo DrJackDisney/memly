e3abd88f55ccc386e34a607ca767e5700be1c5c9
inaba_t <inaba_t@worksap.co.jp>
2017-01-13 23:29:31 +0900

    add globalnavi

--- index.php
+++ index.php
@@ -2,9 +2,20 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<!-- css -->
+<link rel="stylesheet" type="text/css" href="css/common.css">
+
 <title>Memly</title>
 </head>
-<body>
+<body class="bk-shironeri">
+<div class="global-nav bk-sohi">
+	<div class="global-inner">
+		<div class="gloval-nav-center">
+			<img class="img-logo"src="img/logo.png" />
+		</div>
+	</div>
+</div>
 <?php
 $message = "Hello Memly";
 echo "<h1>$message</h1>";
